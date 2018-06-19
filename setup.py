ff09c6616467ae7361d6b26b6997d7b5c365d3ca
Zhirui Wang <576572949@qq.com>
2018-06-19 16:39:26 -0400

    Revert "Update to 0.3.1"
    This reverts commit ecf33d2337095707a6cafc2d5bdc411948e2e46f.

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 
 
 setup(name='pandleau',
-      version='0.3.1',
+      version='0.3',
       packages=find_packages(exclude=['tests*']),
       license='MIT',
       description='A quick and easy way to convert a Pandas DataFrame to a Tableau extract.',
       long_description=open('README.md').read(),
       install_requires=['pandas','numpy','tableausdk'],
-      author='Benjamin Wiley <bewi7122@colorado.edu>, Zhirui(Jerry) Wang <zw2389@columbia.edu>',
+      author=['Benjamin Wiley','Zhirui(Jerry) Wang'],
+      author_email=['bewi7122@colorado.edu','zw2389@columbia.edu'],
       url='https://github.com/bwiley1/pandleau',
-      download_url='https://github.com/bwiley1/pandleau/dist/pandleau-0.3.1.tar.gz',
+      download_url='https://github.com/bwiley1/pandleau/dist/pandleau-0.3.tar.gz',
       py_modules=['pandleau'],
       keywords='tableau pandas extract tde hyper',
       classifiers=['Programming Language :: Python'])
