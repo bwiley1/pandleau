1841213c12a9af528ea7e43f5dfc5ecf203880f5
Daniel Key <daniel@33n.co.uk>
2019-04-16 14:39:57 +0100

    Formatting cleanup

--- pandleau/pandleau.py
+++ pandleau/pandleau.py
@@ -11,25 +11,25 @@ from tableausdk import *
 from tqdm import tqdm
 
 try:
-	from tableausdk.Extract import *
+    from tableausdk.Extract import *
 
-	print("You are using the Tableau SDK, please save the output as .tde format")
-except:
-	pass
+    print("You are using the Tableau SDK, please save the output as .tde format")
+except ModuleNotFoundError:
+    pass
 
 try:
-	from tableausdk.HyperExtract import *
+    from tableausdk.HyperExtract import *
 
-	print("You are using the Extract API 2.0, please save the output as .hyper format")
-except:
-	pass
+    print("You are using the Extract API 2.0, please save the output as .hyper format")
+except ModuleNotFoundError:
+    pass
 
 
-class pandleau( object ):
-    '''
+class pandleau(object):
+    """
     Modification to the pandas DataFrame object
-    
-    '''
+
+    """
     mapper = {'string': Type.UNICODE_STRING,
               'bytes': Type.BOOLEAN,
               'floating': Type.DOUBLE,
@@ -64,55 +64,54 @@ class pandleau( object ):
                     Type.DURATION: lambda row, entry_index, entry: row.setDuration(entry_index, entry.day, entry.hour,
                                                                                    entry.minute,
                                                                                    entry.second, entry.microsecond)}
-    
+
     @staticmethod
     def data_static_type(column):
-            '''
+        """
             Translates pandas datatypes to static datatypes (for initial columns)
             @param column is dataframe column
-            
-            '''
 
-            try:
-                # Use pandas api for inferring types for latest versions of pandas, lib method for earlier versions
-                if pandas.__version__ >= '0.21.0':
-                    return pandleau.mapper[pandas.api.types.infer_dtype(column.dropna())]
-                else:
-                    return pandleau.mapper[pandas.lib.infer_dtype(column.dropna())]
-            except:
-                raise Exception('Error: Unknown pandas to Tableau data type.')
-    
-    @classmethod 
-    def __init__( self, dataframe ):
+            """
+
+        try:
+            # Use pandas api for inferring types for latest versions of pandas, lib method for earlier versions
+            if pandas.__version__ >= '0.21.0':
+                return pandleau.mapper[pandas.api.types.infer_dtype(column.dropna())]
+            else:
+                return pandleau.mapper[pandas.lib.infer_dtype(column.dropna())]
+        except:
+            raise Exception('Error: Unknown pandas to Tableau data type.')
+
+    def __init__(self, dataframe):
         if dataframe.__class__.__name__ != 'DataFrame':
             raise Exception('Error: object is not a pandas DataFrame.')
-        
+
         self._dataframe = dataframe.reset_index()
         self._column_names = list(self._dataframe.columns)
-        
+
         # Initial column types
-        self._column_static_type = self._dataframe.apply(lambda x: pandleau.data_static_type(x), axis = 0)
+        self._column_static_type = self._dataframe.apply(lambda x: pandleau.data_static_type(x), axis=0)
         self._column_set_function = [
             pandleau.entry_writer.get(col_type, lambda row, entry_index, _: row.setNull(entry_index))
             for col_type in
             self._column_static_type]
 
-    def set_spatial(self, column_index, indicator = True):
-        '''
+    def set_spatial(self, column_index, indicator=True):
+        """
         Allows the user to define a spatial column
-        @param column_name = index of spatial column,
+        @param column_index = index of spatial column,
                              either number or name
-        @param indicator = change spatial characteristic  
-        
-        '''
-        
+        @param indicator = change spatial characteristic
+
+        """
+
         if indicator:
             if column_index.__class__.__name__ == 'int':
                 self._column_static_type[column_index] = Type.SPATIAL
             elif column_index.__class__.__name__ == 'str':
                 self._column_static_type[self._column_names.index(column_index)] = Type.SPATIAL
             else:
-                raise Exception ('Error: could not find column in dataframe.')
+                raise Exception('Error: could not find column in dataframe.')
         else:
             if column_index.__class__.__name__ == 'int':
                 self._column_static_type[column_index] = pandleau.data_static_type(
@@ -121,16 +120,16 @@ class pandleau( object ):
                 self._column_static_type[self._column_names.index(column_index)] = pandleau.data_static_type(
                     self._dataframe.loc[:, column_index])
             else:
-                raise Exception ('Error: could not find column in dataframe.')
+                raise Exception('Error: could not find column in dataframe.')
 
     def to_tableau(self, path, table_name='Extract', add_index=False):
-        '''
+        """
         Converts a Pandas DataFrame to a Tableau .tde file
         @param path = path to write file
         @param table_name = name of the table in the extract
         @param add_index = adds incrementing integer index before dataframe columns
 
-        '''
+        """
 
         # Delete Extract and debug log is already exist
         for file in [path, os.path.dirname(path) + '/debug.log',
@@ -139,10 +138,10 @@ class pandleau( object ):
                 os.remove(file)
 
         # Create Extract and Table
-        ExtractAPI.initialize( )
-        new_extract = Extract( path )
+        ExtractAPI.initialize()
+        new_extract = Extract(path)
         table_def = TableDefinition()
-        
+
         # Set columns in Tableau
         if add_index:
             index_col = 'index'
@@ -151,28 +150,28 @@ class pandleau( object ):
                 index_col = '%s_%s' % (index_col, suffix)
                 suffix += 1
             table_def.addColumn(index_col, Type.INTEGER)
-            
+
         for col_index, col_name in enumerate(self._dataframe):
-            table_def.addColumn( col_name, self._column_static_type[col_index] )
-        
+            table_def.addColumn(col_name, self._column_static_type[col_index])
+
         # Create table
         new_table = new_extract.addTable(table_name, table_def)
-        
+
         # Set Column values
-        self.set_column_values( new_table, table_def, add_index )
-        
+        self.set_column_values(new_table, table_def, add_index)
+
         # Close extract
         new_extract.close()
         ExtractAPI.cleanup()
-    
-    def set_column_values( self, tableau_table, extract_table, add_index ):
-        '''
+
+    def set_column_values(self, tableau_table, extract_table, add_index):
+        """
         Translates pandas datatypes to tableau datatypes
-        
-        '''
+
+        """
         # Create new row
-        new_row = Row( extract_table )
-        cols_range = range(len(self._dataframe.columns)+add_index)
+        new_row = Row(extract_table)
+        cols_range = range(len(self._dataframe.columns) + add_index)
 
         for row_index in tqdm(self._dataframe.itertuples(index=add_index), desc='processing table'):
 
@@ -181,7 +180,7 @@ class pandleau( object ):
                 if col_index == 0 and add_index:
                     new_row.setInteger(col_index, col_entry)
                 else:
-                    column_set_function = self._column_set_function[col_index-add_index]
+                    column_set_function = self._column_set_function[col_index - add_index]
 
                     try:
                         if pandas.isnull(col_entry):
@@ -190,26 +189,26 @@ class pandleau( object ):
                             column_set_function(new_row, col_index, col_entry)
                     except:
                         new_row.setNull(col_index)
-                
-            tableau_table.insert( new_row )
-    
+
+            tableau_table.insert(new_row)
+
     @staticmethod
     def determine_entry_value(new_row, entry_index, entry, column_type):
-        '''
+        """
         Determines the entry value
         @param new_row is the new row of the Tableau extract
         @param entry_index is the index of the entry
         @param entry is an entry of the dataframe
         @param column_type is the data type of the corresponding entry column
-        
-        '''
-        
+
+        """
+
         try:
             if pandas.isnull(entry):
                 new_row.setNull(entry_index)
             else:
-                pandleau.entry_writer.get(column_type, lambda row, entry_index, _: row.setNull(entry_index))\
-                    (new_row, entry_index, entry)
+                (pandleau.entry_writer
+                 .get(column_type, lambda row, index, _: row.setNull(index))(new_row, entry_index, entry))
 
         except:
             new_row.setNull(entry_index)
